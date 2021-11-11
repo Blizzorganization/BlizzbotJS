db39b5a8790346396604a8d52661bc98534f5d88
BlackDemonFire <42120952+BlackDemonFire@users.noreply.github.com>
2021-11-11 09:28:45 +0100

    updated help.js

--- commands/text/commands/help.js
+++ commands/text/commands/help.js
@@ -12,8 +12,8 @@ async function run(client, message) {
         .setTitle("**__Der Bot kann folgende Befehle__**")
         .setColor(0xedbc5d)
         .addField("!mc [Name]", "Registriere deinen Minecraft-Account")
-        .addField("mcname [Name]", "Gibt deinen aktuellen Minecraft-Account wieder")
-        .addField("rank [Name]", "Gibt Erfahrung wieder")
+        .addField("!mcname [Name]", "Gibt deinen aktuellen Minecraft-Account wieder")
+        .addField("!rank [Name]", "Gibt Erfahrung wieder")
         .addField("!anfrage", "Schreibe dem Bot eine Anfrage, die direkt an die Moderatoren privat weitergeleitet werden");
     message.channel.send({ embeds: [embed] });
 }
