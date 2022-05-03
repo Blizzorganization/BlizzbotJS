48f5f11cf4da45c32c09a235b75f01b3bc936963
BlackDemonFire <lucy@blackdemon.codes>
2022-05-03 14:19:22 +0200

    fix

--- modules/DiscordClient.js
+++ modules/DiscordClient.js
@@ -54,7 +54,7 @@ class Client extends discord.Client {
         loadCommands("commands/text/functions", this.commands);
         loadCommands("commands/text/ccmds", this.commands);
         loadCommands("commands/slash/user", this.slashCommands);
-        loadCommands("commands/slash/moderation", this.slashCommands);
+        // loadCommands("commands/slash/moderation", this.slashCommands);
         loadCommands("commands/slash/dev", this.slashCommands);
         loadEvents(this, "events");
         if (process.env.DEBUG === "2") this.on("debug", m => { logger.debug(m); });
