398ae23bf28fbd76212185895e1c712831acda63
BlackDemonFire <lucy@blackdemon.codes>
2022-05-07 10:08:16 +0200

    fix(automod): ignore messages on updates too when ignored normally

--- events/messageEdit.js
+++ events/messageEdit.js
@@ -1,3 +1,4 @@
+import logger from "../modules/logger.js";
 import { checkMessage } from "./message.js";
 
 /**
@@ -6,6 +7,14 @@ import { checkMessage } from "./message.js";
  * @param  {import("discord.js").Message} newMessage
  */
 export async function handle(client, oldMessage, newMessage) {
+    logger.silly("message edit received");
+    if (!newMessage) return;
+    logger.silly("message exists");
+    if (newMessage.partial) newMessage = await newMessage.fetch();
+    logger.silly("fetched possible partial message");
+    // ignore webhooks
+    if (newMessage.author.discriminator === "0000") return;
+    if (client.config.channels.ignore.includes(newMessage.channelId)) return;
     checkMessage(client, newMessage);
 }
 
