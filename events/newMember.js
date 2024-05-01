02af3a250ea8744a2641963b445b3189e97259ab
Ratzifutzi <71138238+Ratzifutzi@users.noreply.github.com>
2024-05-01 18:30:16 +0200

    Update newMember.js
    Fixed whitespaces for newMember message.

--- events/newMember.js
+++ events/newMember.js
@@ -5,8 +5,8 @@
 export async function handle(client, member) {
     member.send({
         content: `Willkommen auf Blizzor's Community Server.
-        Damit du auf dem Server freigeschalten wirst, musst du den Befehl ${client.config.prefix}zz verwenden.
-        Bitte gib diesen Befehl im Channel #freischalten ein.`,
+Damit du auf dem Server freigeschalten wirst, musst du den Befehl ${client.config.prefix}zz verwenden.
+Bitte gib diesen Befehl im Channel #freischalten ein.`,
     });
 }
 export const disabled = false;
