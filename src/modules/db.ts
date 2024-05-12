c5d222d136a7d5e95e9040fd2dd150b69cb1ef11
BlackDemonFire <lucy@amogus.cloud>
2024-05-12 22:32:49 +0200

    organize imports

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import config from "./config";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 export const sql = postgres(config.database);
 export const db = drizzle(sql, { logger: true });
